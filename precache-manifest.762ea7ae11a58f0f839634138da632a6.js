a6a1814186da120ff8204bfd28b06a6b2339b94e
SavostinIllia <savostin.illia@gmail.com>
2020-02-11 21:01:22 +0100

    Updates

---
+++ precache-manifest.762ea7ae11a58f0f839634138da632a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6de17adb68bf9917381ebe702ef121a",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "cc1e6311c3588fa47538",
+    "url": "/todo-react/static/css/main.e27b51a0.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/todo-react/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-react/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1e6311c3588fa47538",
+    "url": "/todo-react/static/js/main.2f47f1af.chunk.js"
+  },
+  {
+    "revision": "8c8404ddc91811c2534b",
+    "url": "/todo-react/static/js/runtime-main.c528bf7d.js"
+  }
+]);
